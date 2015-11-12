fcc5ce765b15b9dd4cbdba250d6dd59fce70dc4d
Nicola Nauwynck <nicola.nauwynck@district01.be>
2015-11-12 16:54:31 +0100

    Added fixtures for taxonomy

--- _nosql/fixtures/importDevFixtures.sh
+++ _nosql/fixtures/importDevFixtures.sh
@@ -9,7 +9,8 @@ SERVER=127.0.0.1:27017
 # The fixtures that will be imported into the database.
 FIXTURES=(
     languages,
-    config
+    config,
+    taxonomy
 )
 
 # Run the mongoImport command n-times

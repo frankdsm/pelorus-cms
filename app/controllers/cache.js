648d28a937346c3634c2928a29d0269c7298298c
Thomas Bormans <thomas.bormans@district01.be>
2015-11-12 11:10:30 +0100

    Mongo Cache added

---
+++ app/controllers/cache.js
@@ -0,0 +1,62 @@
+'use strict';
+
+require('rootpath')();
+var Cache = require('app/models/cache');
+
+exports.set = function (key, value, callback) {
+    Cache.update({key: key}, { key: key, value: value, created: new Date() }, {upsert:true})
+        .exec(function(err, update) {
+            if(!err && update) {
+                if(callback) {
+                    callback();
+                }
+            } else {
+                if(callback) {
+                    callback(err);
+                }
+            }
+        });
+};
+
+exports.get = function (key, expiresTime, callback) {
+    Cache.findOne({key: key})
+        .exec(function(err, item) {
+            if(!err && item) {
+                if(!expiresTime) {
+                    expiresTime = 10;
+                }
+                var startdate = new Date(item.created);
+                var expireDate = new Date(startdate.getTime() + (1000 * expiresTime));
+                var now = new Date();
+                if(expireDate < now) {
+                    if(callback) {
+                        callback(false);
+                    }
+                } else {
+                    if(callback) {
+                        callback(false, item.value);
+                    }
+                }
+            } else {
+                if(callback) {
+                    callback(err);
+                }
+            }
+        });
+};
+
+exports.remove = function (key, callback) {
+    Cache.findOne({key: key})
+        .remove()
+        .exec(function(err, result) {
+            if(!err && result) {
+                if(callback) {
+                    callback();
+                }
+            } else {
+                if(callback) {
+                    callback(err);
+                }
+            }
+        });
+};

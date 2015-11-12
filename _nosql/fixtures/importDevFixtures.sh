44ea4e622ea90e3b97c7080a3cece50c3826bd4f
Nicola Nauwynck <nicola.nauwynck@district01.be>
2015-11-12 12:10:49 +0100

    - shell script for fixtures

---
+++ _nosql/fixtures/importDevFixtures.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# This script loads the fixtures with data into your current mongo-db.
+
+# The database to run the import command on.
+DATABASE=pelorus-dev
+SERVER=127.0.0.1:27017
+
+# The fixtures that will be imported into the database.
+FIXTURES=(
+    languages
+)
+
+# Run the mongoImport command n-times
+# Upserts the collection data using "--upsert"
+# Drop is possible using "--drop"
+# ---
+# https://docs.mongodb.org/manual/reference/program/mongoimport/
+for fixture in ${FIXTURES[@]}
+do
+    mongoimport --jsonArray -h $SERVER -d $DATABASE -c $fixture --drop --file json/$fixture.json
+done

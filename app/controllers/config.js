6ea2d6872d90918e40fdcb1eb0b56e0df092ac72
Nicola Nauwynck <nicola.nauwynck@district01.be>
2015-11-12 16:04:42 +0100

    - update the settings of the config

--- app/controllers/config.js
+++ app/controllers/config.js
@@ -32,6 +32,50 @@ var readOne = function (req, res, next) {
 };
 exports.readOne = readOne;
 
+
+/**
+ * @api {put} /api/config/:type Update config file
+ * @apiParam {String} type Config file type
+ * @apiGroup Config
+ * @apiVersion 0.0.1
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "config": {},
+ *       "type": "xxxx",
+ *       "uuid": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+ *       "id": "xxxxxxxxxxxxxxxxxxxxxxxx"
+ *     }
+ */
 exports.update = function (req, res, next) {
-    return null
+    // Compatibility fix for old MongoDB versions
+    delete req.body._id;
+    // Update version before save
+    versions.add(Config, req.body, function(data) {
+        Config.findOneAndUpdate({type: req.params.stype}, data)
+            .then(
+                function onSuccess(oldObject) {
+                    // Check if siteHome exists in the req object
+                    if (!req.body.data.hasOwnProperty('siteHome')) {
+                        return;
+                    }
+
+                    if (oldObject.data.siteHome !== req.body.data.siteHome._id) {
+                        // To do: set the new homepage
+                        return;
+                    }
+                },
+                function onError(updateError) {
+                    res.status(400).json({err: updateError});
+                    return updateError;
+                }
+            ).then(
+                function onSuccess(err) {
+                    if(!err) {
+                        return readOne(req, res, next);
+                    }
+                }
+            );
+    });
 };
